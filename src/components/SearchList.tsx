2ed9c5d7076be4bfb6ed9d3fc4f1df97b1c01275
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2023-03-24 00:06:08 +0100

    Updated filters

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -3,21 +3,29 @@ import React, { useState } from 'react';
 export type Hook = {
     name: string,
     type: string,
-
+    summery: string,
 }
 const SearchList = ({list}: { list: Hook[] }) => {
     const [currentList, setCurrentList] = useState(list);
 
-    return <div>
-        <input onChange={event => {
+    return <div className="search-list">
+        <input
+            placeholder="Search hook"
+            onChange={event => {
             const value = event.target.value;
             const newList = list.filter(e => e.name.includes(value));
             setCurrentList(newList);
         }}/>
         <table>
+            <tr>
+                <th>Hook</th>
+                <th>Type</th>
+                <th>Description</th>
+            </tr>
             {currentList.map(element => <tr>
                 <td><a href={`/${element.name}`}>{element.name}</a></td>
                 <td>{element.type}</td>
+                <td>{element.summery}</td>
             </tr>)}
         </table>
     </div>
